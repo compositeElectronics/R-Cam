db7c038eecb2b493e48d36871367476034913922
Tim Brocklehurst <tim@compositeelectronics.com>
2017-09-17 12:12:41 +0100

    Fixed bug when surfaces have singularities. Singularity is ignored. This typically is a good enough solution.

--- src/surfacingToolpath.cpp
+++ src/surfacingToolpath.cpp
@@ -2,6 +2,9 @@
 
 surfacingToolpath::surfacingToolpath(rcamObject *parent) : genericToolpath(parent, "surfacingToolpath"){
   setting.append(new editableSetting(QString("zStep"), QString("Z Step"),   QVariant((double)-0.5), QVariant(-100), QVariant(-0.0001)));
+  setting.append(new editableSetting(QString("zFeed"), QString("Z Feed"),   QVariant((double)50), QVariant(1), QVariant(1000)));
+  setting.append(new editableSetting(QString("toolRadius"), QString("Tool Radius"),   QVariant((double)3), QVariant(0), QVariant(100)));
+  setting.append(new editableSetting(QString("srfOffset"), QString("Surface Offset"),   QVariant((double)0), QVariant(-100), QVariant(100)));
   createSettingsTable();
   createMenu();
 }
@@ -25,9 +28,9 @@ void surfacingToolpath::calcToolPath(const ON_Surface* srf, geomReference* geomR
   double u, v, umin, umax, vmin, vmax, uStep, vStep, toolRadius;
   char line[256];
   
-  bool takeNextCut;
-  double z, zStep, zi;
-  bool ramp, stepDown;
+  bool takeNextCut, stepU, stepV;
+  double z, zStep, zi, zFeed, srfOffset;
+  bool ramp, stepDown, flipNormals, isFirst, isRapid;
   QVariant setting;
   
   ON_3dPoint srfPt, toolPt, toolDelta;
@@ -36,10 +39,12 @@ void surfacingToolpath::calcToolPath(const ON_Surface* srf, geomReference* geomR
   srf->GetDomain(0, &umin, &umax);
   srf->GetDomain(1, &vmin, &vmax);
   
-  toolRadius=3;
+  toolRadius=findSettingValue("toolRadius").toDouble();
+  srfOffset=findSettingValue("srfOffset").toDouble();
   toolDelta=ON_3dPoint(0, 0, -toolRadius);
   
   zStep=findSettingValue("zStep").toDouble(); 
+  zFeed=findSettingValue("zFeed").toDouble();
   
   uStep=(umax-umin)/10.;
   vStep=(vmax-vmin)/10.;
@@ -48,32 +53,72 @@ void surfacingToolpath::calcToolPath(const ON_Surface* srf, geomReference* geomR
   setting=geomRef->findSettingValue("stepV"); if (setting!=QVariant()) vStep=setting.toDouble();
   
   stepDown=geomRef->findSettingValue("stepDown").toBool();
+  flipNormals=geomRef->findSettingValue("flipNormals").toBool();
+
+  sprintf(line,"#<zFeed>=%lf",zFeed); path.append(line);
+  path.append("F #<stdFeed>");
   
-  z=0;
+  z=findSettingValue("startZ").toDouble();
   if (!stepDown) z=-1000;
   takeNextCut=true;
+  isRapid=false;
   while (takeNextCut){
     z+=zStep;
     takeNextCut=false;
-//    printf("Z=%lf , uStep=%lf , vStep=%lf\n",z,uStep, vStep);
-    for (u=umin; u<=umax+.001; u+=uStep){
-      srfPt=srf->PointAt(u,vmin);
-      normal=srf->NormalAt(u,vmin);
-      toolPt=srfPt+normal*toolRadius+toolDelta;
-      sprintf(line,"G0 X%lf Y%lf",toolPt.x, toolPt.y);
-      path.append(line);
-      path.append("G0 Z[#<workZ>+#<rapidZ>]");
-      for (v=vmin; v<=vmax+.001; v+=vStep){
+    printf("uMin=%lf , uMax=%lf\n", umin, umax);
+    printf("vMin=%lf , vMax=%lf\n", vmin, vmax);
+    printf("Z=%lf , uStep=%lf , vStep=%lf\n",z,uStep, vStep);
+    u=umin;
+    stepU=true;
+    while (stepU){
+      if (u>umax){ u=umax; stepU=false; }
+      
+      isFirst=true;
+      v=vmin;
+      stepV=true;
+      while (stepV){
+        if (v>vmax){ v=vmax; stepV=false; }
 
         srfPt=srf->PointAt(u,v);
         normal=srf->NormalAt(u,v);
       
-        toolPt=srfPt+normal*toolRadius+toolDelta;
+        if (srf->IsAtSingularity(u,v,false)){
+          printf("Ignoring surface point %lf,%lf - point is a singularity\n",u,v);
+          v+=vStep;
+          continue;
+        }
+    
+        if (!flipNormals){
+          toolPt=srfPt+normal*(toolRadius+srfOffset);
+        }else{
+          toolPt=srfPt-normal*(toolRadius+srfOffset);
+        }
+      
+        toolPt+=toolDelta;
       
 //        printf("     xyz = %lf %lf %lf\n", srfPt.x, srfPt.y, srfPt.z);
 //        printf("  normal = %lf %lf %lf\n", normal.x, normal.y, normal.z);
 //        printf("tool xyz = %lf %lf %lf\n", toolPt.x, toolPt.y, toolPt.z);
       
+        
+        if (stepDown && toolPt.z>z+zStep){
+          if (!isRapid){
+            printf("into rapid - u=%lf\n",u);
+            path.append("G0 Z[#<workZ>+#<rapidZ>]");
+            isRapid=true;
+          }
+          v+=vStep;
+          continue;
+        }
+        
+        if (isRapid){
+          printf("out of rapid - u=%lf\n",u);
+          sprintf(line,"G0 X%lf Y%lf",toolPt.x, toolPt.y);
+          path.append(line);
+          isFirst=true;
+        }
+        isRapid=false;
+        
         sprintf(line,"G1 X%lf Y%lf",toolPt.x, toolPt.y);
         if (toolPt.z>z){
           sprintf(line,"%s Z[#<workZ>+%lf]", line, toolPt.z);
@@ -82,9 +127,32 @@ void surfacingToolpath::calcToolPath(const ON_Surface* srf, geomReference* geomR
           takeNextCut=true;
 //          printf("Must take next cut\n");
         }
-        path.append(line);
+        
+        if (isFirst){
+          sprintf(line,"G0 X%lf Y%lf",toolPt.x, toolPt.y);
+          path.append(line);
+          if (toolPt.z>z){
+            sprintf(line,"G0 Z[#<workZ>+%lf]", line, toolPt.z-zStep);
+          }else{
+            sprintf(line,"G0 Z[#<workZ>+%lf]", line, z-zStep);
+          }
+          path.append(line);
+          path.append("F #<zFeed>");
+          if (toolPt.z>z){
+            sprintf(line,"G1 Z[#<workZ>+%lf]", line, toolPt.z);
+          }else{
+            sprintf(line,"G1 Z[#<workZ>+%lf]", line, z);
+          }
+          path.append(line);
+          path.append("F #<stdFeed>");
+          isFirst=false;
+        }else{
+          path.append(line);
+        }
+        v+=vStep;
       }
       path.append("G0 Z[#<workZ>+#<rapidZ>]");
+      u+=uStep;
     }
   }
 }
